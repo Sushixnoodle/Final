c89ae948d6da125fda2632b369955c157699a782
Sushixnoodle <145032867+Sushixnoodle@users.noreply.github.com>
2025-03-14 11:52:14 +0000

    7:18

---
+++ Assets/Scripts/DialogyeThree/DialogueGraph.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.EditorCoroutines.Editor;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+public class DialogueGraph : EditorWindow
+{
+
+    private DialogueGraphView _graphView;
+
+    [MenuItem("Graph/DialogueGraph")]
+    public static void OpenDialogueGraphWindow()
+    {
+        var window = GetWindow<DialogueGraph>();
+        window.titleContent = new GUIContent(text: "Dialogue Graph");
+    }
+
+    private void OnEnable()
+    {
+        _graphView = new DialogueGraphView
+        {
+            name = "Dialogue Graph"
+        };
+
+        _graphView.StretchToParentSize();
+        rootVisualElement.Add(_graphView);
+    }
+    private void OnDisable()
+    {
+        rootVisualElement.Remove(_graphView);
+    }
+}
