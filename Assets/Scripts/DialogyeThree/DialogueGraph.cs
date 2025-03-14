0d752ba4ee72a2466ac16bb48681366a6137b594
Sushixnoodle <145032867+Sushixnoodle@users.noreply.github.com>
2025-03-14 12:46:13 +0000

    diallohgeu

--- Assets/Scripts/DialogyeThree/DialogueGraph.cs
+++ Assets/Scripts/DialogyeThree/DialogueGraph.cs
@@ -4,6 +4,7 @@ using Unity.EditorCoroutines.Editor;
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.UIElements;
+using UnityEditor.UIElements;
 
 public class DialogueGraph : EditorWindow
 {
@@ -19,6 +20,12 @@ public class DialogueGraph : EditorWindow
 
     private void OnEnable()
     {
+        ConstructGraphView();
+        GenerateToolbar();
+    }
+
+    private void ConstructGraphView()
+    {
         _graphView = new DialogueGraphView
         {
             name = "Dialogue Graph"
@@ -27,6 +34,19 @@ public class DialogueGraph : EditorWindow
         _graphView.StretchToParentSize();
         rootVisualElement.Add(_graphView);
     }
+
+    private void GenerateToolbar()
+    {
+        var toolbar = new Toolbar();
+
+        var nodeCreateButton = new Button(clickEvent: () => { _graphView.CreateDialogueNode("Dialogue Node"); });
+        nodeCreateButton.text = "Create Node";
+        toolbar.Add(nodeCreateButton);
+
+        rootVisualElement.Add(toolbar);
+
+    }
+
     private void OnDisable()
     {
         rootVisualElement.Remove(_graphView);
