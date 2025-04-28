6b121427eafbe637d014ed7f49b16f56bea777bc
Sushixnoodle <145032867+Sushixnoodle@users.noreply.github.com>
2025-04-28 10:46:33 +0100

    door

--- Assets/doorTrigger.cs
+++ Assets/doorTrigger.cs
@@ -1,31 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class doorTrigger : MonoBehaviour
 {
     public Animator doorAnimator;
-    public float interactionDistance = 3f;
-
-    private Transform player;
     private bool isPlayerInRange = false;
     private bool isOpen = false;
-
-    void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-    }
 
     void Update()
     {
         if (isPlayerInRange && Input.GetKeyDown(KeyCode.E))
         {
-            float distance = Vector3.Distance(player.position, transform.position);
-            if (distance <= interactionDistance)
-            {
-                isOpen = !isOpen;
-                doorAnimator.SetBool("isOpen", isOpen);
-            }
+            isOpen = !isOpen;
+            doorAnimator.SetBool("isOpen", isOpen);
         }
     }
 
