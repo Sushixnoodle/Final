fbf32834b0c2ba1edd0fe7882e2e874b9e55ef2a
Sushixnoodle <145032867+Sushixnoodle@users.noreply.github.com>
2025-05-06 15:19:39 +0100

    idek revert it

--- Assets/MaterialAnimator.cs
+++ Assets/MaterialAnimator.cs
@@ -3,20 +3,41 @@ using UnityEngine;
 
 public class MaterialAnimator : MonoBehaviour
 {
-    public Renderer[] objects; // Assign objects left to right
-    public Material targetMaterial; // The material to apply
-    public float delayBetween = 0.5f; // Time between each material change
+    public Renderer[] objects; // Assign left-to-right
+    public float delayBetween = 0.5f;
+    public float wipeDuration = 1f;
 
     void Start()
     {
-        StartCoroutine(AnimateMaterials());
+        StartCoroutine(WipeInMaterials());
     }
 
-    IEnumerator AnimateMaterials()
+    IEnumerator WipeInMaterials()
     {
         foreach (Renderer obj in objects)
         {
-            obj.material = targetMaterial;
+            Material mat = obj.material;
+
+            // Make a unique instance of the material
+            obj.material = new Material(mat);
+
+            // Reset offset
+            obj.material.mainTextureScale = new Vector2(1, 1);
+            obj.material.mainTextureOffset = new Vector2(-1, 0);
+
+            float timer = 0f;
+            while (timer < wipeDuration)
+            {
+                float progress = timer / wipeDuration;
+                obj.material.mainTextureOffset = new Vector2(-1 + progress, 0);
+                obj.material.mainTextureScale = new Vector2(progress, 1);
+                timer += Time.deltaTime;
+                yield return null;
+            }
+
+            obj.material.mainTextureOffset = Vector2.zero;
+            obj.material.mainTextureScale = Vector2.one;
+
             yield return new WaitForSeconds(delayBetween);
         }
     }
